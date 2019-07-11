748287818aee69220d97a30a00467a98a2597db5
Toshiuk <nara_toshiraka@hotmail.com>
2019-07-11 00:21:05 -0300

    Add Footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 import './Footer.css'
 
-export default function Footer () {
+export default function Footer() {
   return (
-    <footer id='main-footer'>
-      <div className='footer-content'>
-        <a>Logo</a>
+    <footer id="main-footer" className="footer">
+      <div className="content footer-content">
+        <a href="https://github.com/Toshiuk">
+          Github
+      </a>
+        <a href="https://www.linkedin.com/in/flaviotoshiukhjr/">
+          LinkedIn
+      </a>
       </div>
     </footer>
   )
